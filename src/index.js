1f1bbd2a90d4b945c9149f445858f2e38ca943aa
Simon Larsén <slarse@slar.se>
2021-04-16 12:48:33 +0200

    Add first draft of buildbreaker action

--- src/index.js
+++ src/index.js
@@ -1,14 +1,54 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+const fs = require("fs");
+const got = require("got");
+const stream = require("stream");
+const {promisify} = require("util");
+
+const sorald = require('./sorald');
+
+const pipeline = promisify(stream.pipeline);
+
+async function download(url, dst) {
+  return pipeline(
+    got.stream(url),
+    fs.createWriteStream(dst)
+  );
+}
+
+async function runSorald(source, soraldJarUrl, ) {
+  const jarDstPath = "sorald.jar"
+
+  console.log(`Downloading Sorald jar to ${jarDstPath}`);
+  await download(soraldJarUrl, jarDstPath);
+  console.log(`Mining rule violations at ${source}`);
+  keyToSpecs = await sorald.mine(jarDstPath, source, "stats.json");
+
+  console.log('Found rule violations');
+
+  console.log('Attempting repairs');
+  const performedRepairs = Array.from(keyToSpecs.entries()).flatMap(async function(subArray) {
+    const [ruleKey, violationSpecs] = subArray;
+    console.log(`Repairing violations of rule ${ruleKey}: ${violationSpecs}`);
+    const statsFile = `${ruleKey}.json`;
+    const repairs = await sorald.repair(jarDstPath, source, statsFile, violationSpecs);
+    sorald.restore(source);
+    return repairs;
+  });
+
+  return (await Promise.all(performedRepairs)).flatMap(e => e);
+}
 
 try {
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-
-  const payload = JSON.stringify(github.context.payloud, undefined, 2);
-  console.log(`The event payload: ${payload}`);
+  const source = core.getInput('source');
+  const soraldJarUrl = core.getInput('sorald-jar-url')
+  runSorald(source, soraldJarUrl).then(repairedViolations => {
+    if (repairedViolations.length > 0) {
+      const msg = `Found repairable violations ${repairedViolations.join(' ')}`
+      console.log(msg)
+      core.setFailed(msg)
+    }
+  });
 } catch (error) {
   core.setFailed(error.message);
 }
---
+++ src/sorald.js
@@ -0,0 +1,97 @@
+const {promisify} = require('util');
+const bl = require('bl');
+const spawn = require('await-spawn');
+const fs = require('fs');
+
+async function mine(soraldJar, source, statsFile) {
+  try {
+    await spawn(
+      'java',
+      [
+        '-jar',
+        soraldJar,
+        "mine",
+        "--source",
+        source,
+        "--stats-output-file",
+        statsFile,
+        "--handled-rules"
+      ]
+    );
+  } catch (e) {
+    console.log(e.stderr.toString());
+    throw e;
+  }
+
+  const miningData = JSON.parse(await fs.promises.readFile(statsFile))
+  const keyToSpecs = 
+    miningData.minedRules.map(
+      data => [
+        data.ruleKey,
+        data.warningLocations.map(loc => loc.violationSpecifier)
+      ]);
+
+  return new Map(keyToSpecs);
+}
+
+async function repair(soraldJar, source, statsFile, violationSpecs) {
+  try {
+    await spawn(
+      'java',
+      [
+        '-jar',
+        soraldJar,
+        'repair',
+        '--source',
+        source,
+        '--stats-output-file',
+        statsFile,
+        '--violation-specs',
+        violationSpecs.join(','),
+      ]
+    );
+  } catch (e) {
+    console.log(e.stderr.toString());
+    throw e;
+  }
+
+  const repairData = JSON.parse(await fs.promises.readFile(statsFile));
+  return parseRepairedViolations(repairData);
+}
+
+async function restore(source) {
+  try {
+    await spawn(
+      'git',
+      [
+        'restore',
+        source,
+      ],
+      {cwd: source},
+    )
+  } catch (e) {
+    console.log(e.stderr.toString());
+    throw e;
+  }
+}
+
+function parseRepairedViolations(repairData) {
+  ruleRepairs = repairData.repairs
+
+  if (!ruleRepairs) {
+    return [];
+  } else {
+    ruleRepairData = ruleRepairs[0];
+
+    numSuccessfulRepairs = ruleRepairData.nbViolationsBefore - ruleRepairData.nbViolationsAfter;
+    if (numSuccessfulRepairs > 0) {
+      return ruleRepairData.performedRepairsLocations.map(loc => loc.violationSpecifier);
+    } else {
+      return [];
+    }
+  }
+}
+
+module.exports.mine = mine
+module.exports.repair = repair
+module.exports.restore = restore
